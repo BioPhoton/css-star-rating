035dcea9a1f5932f3b1f9537784ad43e103e1024
Michael Hladky <michael@hladky.at>
2016-11-27 16:58:29 +0100

    test(travis): let travis fail

--- chore/gulp/tasks/css.js
+++ chore/gulp/tasks/css.js
@@ -119,7 +119,7 @@ function compile(done) {
     return gulp.src(cssConfig.sassScr)
         .pipe($.concat(cssConfig.mainCssFile))
         //move plumber into helper.js
-        .pipe($.plumber()) // exit gracefully if something fails after this
+        //.pipe($.plumber()) // exit gracefully if something fails after this
         .pipe($.sass(cssConfig.sassOptions).on('error', $.sass.logError))
         .pipe(autoprefixer(cssConfig.autoprefixerOptions))
         .pipe(gulp.dest(cssConfig.sassDest), done);
